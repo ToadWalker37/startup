809498bdee1d391bb1f89d9b99bd12d7de4b2c1e
ToadWalker37 <anthony.christiansen@byu.net>
2024-04-06 18:22:58 -0600

    Made it so vehicles can be added to the database

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ apiRouter.get('/vehicles', (_req, res) => {
 });
 
 apiRouter.post('/vehicle-add', (req, res) => {
-  res.send(JSON.stringify(addVehicles(req.body)));
+  let currentVehicles = addVehicles(req.body);
+  main(currentVehicles[0]).catch(console.error);;
+  res.send(JSON.stringify(currentVehicles));
 });
 
 apiRouter.post('/vehicle-delete', (req, res) => {
@@ -40,4 +42,36 @@ function deleteVehicles(vehicle) {
   vehicles = [];
   for (let vehicleInList of vehiclesIterator) { if (vehicleInList[1].ListingID.localeCompare(vehicle.ListingID) !== 0) { vehicles.push(vehicleInList[1]); } }
   return vehicles;
+}
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const config = require('dbConfig.json');
+
+async function main(input) {
+  const uri = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri);
+  const db = client.db('testDatabase');
+  const testCollection = db.collection('testCollection');
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
+  await testCollection.insertOne(input);
+  const cursor = testCollection.find(query, options);
+  const allItems = await cursor.toArray();
+  allItems.forEach((i) => console.log(i));
 }
