5a45d85bf067f6c691aa62a57ae0bad88a56ba7a
ToadWalker37 <anthony.christiansen@byu.net>
2024-04-17 14:17:00 -0600

    Got most WebSocket code written

--- index.js
+++ index.js
@@ -250,13 +250,66 @@ const { WebSocketServer } = require('ws');
 
 const wss = new WebSocketServer({ port: 9900 });
 
-wss.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    const msg = String.fromCharCode(...data);
-    console.log('received: %s', msg);
+function peerProxy(httpServer) {
+  // Create a websocket object
+  const wss = new WebSocketServer({ noServer: true });
 
-    ws.send(`I heard you say "${msg}"`);
+  // Handle the protocol upgrade from HTTP to WebSocket
+  httpServer.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request);
+    });
   });
 
-  ws.send('Hello webSocket');
+  // Keep track of all the connections so we can forward messages
+  let connections = [];
+
+  wss.on('connection', (ws) => {
+    const connection = { id: uuid.v4(), alive: true, ws: ws };
+    connections.push(connection);
+
+    // Forward messages to everyone except the sender
+    ws.on('message', function message(data) {
+      connections.forEach((c) => {
+        if (c.id !== connection.id) {
+          c.ws.send(data);
+        }
+      });
+    });
+
+    // Remove the closed connection so we don't try to forward anymore
+    ws.on('close', () => {
+      const pos = connections.findIndex((o, i) => o.id === connection.id);
+
+      if (pos >= 0) {
+        connections.splice(pos, 1);
+      }
+    });
+
+    // Respond to pong messages by marking the connection alive
+    ws.on('pong', () => {
+      connection.alive = true;
+    });
+  });
+
+  // Keep active connections alive
+  setInterval(() => {
+    connections.forEach((c) => {
+      // Kill any connection that didn't respond to the ping last time
+      if (!c.alive) {
+        c.ws.terminate();
+      } else {
+        c.alive = false;
+        c.ws.ping();
+      }
+    });
+  }, 10000);
+}
+
+module.exports = { peerProxy };
+
+const httpService = app.listen(8080, () => {
+  console.log(`Listening on port ${8080}`);
 });
+
+peerProxy(httpService);
